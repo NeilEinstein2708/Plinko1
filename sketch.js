5f6d1e41de44a67a54703930f5c7bf90128764e9
NeilEinstein2708 <75164610+NeilEinstein2708@users.noreply.github.com>
2021-04-06 20:27:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,103 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+ 
+var particles = [];
+var plinkos = [];
+var divisions =[];
+var divisionHeight=300;
+var score =0;
+
+function setup() {
+  createCanvas(660, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+  //create division objects
+  for (var k = 0; k <=110; k = k + 110) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+  for (var k = 0; k <=220; k = k + 110) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+
+  for (var k = 0; k <=330; k = k + 110) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+  for (var k = 0; k <=440; k = k + 110) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+  for (var k = 0; k <=550; k = k + 110) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+  for (var k = 0; k <=660; k = k + 110) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+
+
+
+  //create 1st row of plinko objects
+  for (var j = 30; j <=width; j=j+50) { 
+    plinkos.push(new Plinko(j,75));
+  }
+
+  //create 2nd row of plinko objects
+  for (var j = 30; j <=width-10; j=j+50) 
+  {
+    plinkos.push(new Plinko(j,175));
+  }
+
+  for (var j = 30; j <=width; j=j+50) 
+  {
+    plinkos.push(new Plinko(j,275));
+  }
+   
+  for (var j = 30; j <=width - 10; j=j+50) 
+  {
+    plinkos.push(new Plinko(j,375));
+  }
+
+
+
+
+  //create particle objects
+  if(frameCount%60===0){
+    particles.push(new Particle(random(width/2-10, width/2 + 10), 10,10) );
+  }
+    
+}
+ 
+
+
+function draw() {
+  background("black");
+  textSize(20)
+ 
+  Engine.update(engine);
+  ground.display();
+  
+  if(frameCount%60===0){
+    particles.push(new Particle(random(width/2-10, width/2 + 10), 10,10) );
+  }
+    
+
+
+  //display the plinkos 
+  for (var i = 0; i < plinkos.length; i++) {
+    plinkos[i].display();   
+  }
+   
+  //display the divisions
+  for (var k = 0; k < divisions.length; k++) {
+    divisions[k].display();
+  }
+
+
+  for(var j = 0; j < particles.length; j++){
+    particles[j].display();
+  }
+
+  //display the paricles 
+}
